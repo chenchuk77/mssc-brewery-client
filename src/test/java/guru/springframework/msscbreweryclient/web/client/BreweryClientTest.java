08c3d5f09744c14ddc1f81c7226613d76934f08a
chenchuk77 <chenchuk@gmail.com>
2019-08-29 13:11:59 +0300

    done assignment-50. all tests works

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,11 +1,13 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.BeanExpressionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,4 +23,34 @@ class BreweryClientTest {
         BeerDto dto = breweryClient.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
     }
+
+    @Test
+    void testSaveNewCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("jessi").build();
+        URI uri = breweryClient.saveNewCustomer(customerDto);
+        assertNotNull(uri);
+        System.out.println("test returned uri: " + uri);
+    }
+
+    @Test
+    void testUpdateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("tessi").build();
+        breweryClient.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void testDeleteCustomer() {
+        breweryClient.deleteCustomer(UUID.randomUUID());
+    }
+
+    @Test
+    void getCustomerById() {
+        CustomerDto dto = breweryClient.getCustomerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+
+
+
+
 }
